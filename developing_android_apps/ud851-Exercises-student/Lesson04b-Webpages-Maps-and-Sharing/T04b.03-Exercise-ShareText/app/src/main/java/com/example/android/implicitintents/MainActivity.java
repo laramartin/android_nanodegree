8c9e16b0fad2bede5b15adab5d7df590a26ac52c
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-22 19:59:23 +0900

    T04b.03 exercise share text

--- developing_android_apps/ud851-Exercises-student/Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ developing_android_apps/ud851-Exercises-student/Lesson04b-Webpages-Maps-and-Sharing/T04b.03-Exercise-ShareText/app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -23,8 +23,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import static android.R.attr.mimeType;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -63,10 +61,8 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickShareTextButton(View v) {
-        // TODO (5) Specify a String you'd like to share
-
-        // TODO (6) Replace the Toast with shareText, passing in the String from step 5
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        String text = "some text bla bla";
+        shareText(text);
     }
 
     /**
@@ -140,12 +136,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (1) Create a void method called shareText that accepts a String as a parameter
-    // Do steps 2 - 4 within the shareText method
-
-        // TODO (2) Create a String variable called mimeType and set it to "text/plain"
-
-        // TODO (3) Create a title for the chooser window that will pop up
-
-        // TODO (4) Use ShareCompat.IntentBuilder to build the Intent and start the chooser
+    public void shareText(String text) {
+        String mimeType = "text/plain";
+        String title = "how to share";
+        ShareCompat.IntentBuilder.from(this)
+                .setChooserTitle(title)
+                .setType(mimeType)
+                .setText(text)
+                .startChooser();
+    }
 }
