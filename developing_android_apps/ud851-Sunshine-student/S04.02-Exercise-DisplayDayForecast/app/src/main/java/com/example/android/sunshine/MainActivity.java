53d647249f8e6c48bb3eaca9efc2ac9229d048c5
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-22 21:03:34 +0900

    S04.02 exercise display day forecast

--- developing_android_apps/ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ developing_android_apps/ud851-Sunshine-student/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -119,7 +119,7 @@ public class MainActivity extends AppCompatActivity implements ForecastAdapterOn
         Context context = this;
         Class destinationClass = DetailActivity.class;
         Intent intentToStartDetailActivity = new Intent(context, destinationClass);
-        // TODO (1) Pass the weather to the DetailActivity
+        intentToStartDetailActivity.putExtra(Intent.EXTRA_TEXT, weatherForDay);
         startActivity(intentToStartDetailActivity);
     }
 
