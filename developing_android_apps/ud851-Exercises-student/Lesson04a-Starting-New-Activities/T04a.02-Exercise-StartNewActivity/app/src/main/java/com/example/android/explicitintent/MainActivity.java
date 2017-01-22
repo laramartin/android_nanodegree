0faf7aa96b1387441f12628d23fd8dc462f7e988
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-22 16:57:07 +0900

    T04a.02 exercise start new activity

--- developing_android_apps/ud851-Exercises-student/Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ developing_android_apps/ud851-Exercises-student/Lesson04a-Starting-New-Activities/T04a.02-Exercise-StartNewActivity/app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -15,14 +15,13 @@
  */
 package com.example.android.explicitintent;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,16 +58,8 @@ public class MainActivity extends AppCompatActivity {
                  * wanted to demonstrate what parameter we were using "MainActivity.this" for as
                  * clear as possible.
                  */
-                Context context = MainActivity.this;
-
-                // TODO (1) Store ChildActivity.class in a Class object called destinationActivity
-
-                // TODO (2) Create an Intent to start ChildActivity
-
-                // TODO (3) Replace the Toast with code to start ChildActivity
-                String message = "Button clicked!\nTODO: Start a new Activity and pass some data.";
-                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
-
+                Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(intent);
             }
         });
     }
