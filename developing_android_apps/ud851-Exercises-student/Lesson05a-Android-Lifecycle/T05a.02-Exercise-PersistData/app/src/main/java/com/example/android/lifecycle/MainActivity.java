8d627c68ff103dab7978bf555d1871bc4044971d
Lara Martin <lara.martin.carretero@gmail.com>
2017-01-27 16:08:12 +0900

    T05a.02 exercise persist data

--- developing_android_apps/ud851-Exercises-student/Lesson05a-Android-Lifecycle/T05a.02-Exercise-PersistData/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ developing_android_apps/ud851-Exercises-student/Lesson05a-Android-Lifecycle/T05a.02-Exercise-PersistData/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    // TODO (1) Create a key String called LIFECYCLE_CALLBACKS_TEXT_KEY
+    private static final String LIFECYCLE_CALLBACKS_TEXT_KEY = null;
 
     /* Constant values for the names of each respective lifecycle callback */
     private static final String ON_CREATE = "onCreate";
@@ -49,7 +49,9 @@ public class MainActivity extends AppCompatActivity {
 
         mLifecycleDisplay = (TextView) findViewById(R.id.tv_lifecycle_events_display);
 
-        // TODO (6) If savedInstanceState is not null and contains LIFECYCLE_CALLBACKS_TEXT_KEY, set that text on our TextView
+        if (savedInstanceState != null) {
+            mLifecycleDisplay.setText(savedInstanceState.getCharSequence(LIFECYCLE_CALLBACKS_TEXT_KEY));
+        }
 
         logAndAppend(ON_CREATE);
     }
@@ -137,11 +139,12 @@ public class MainActivity extends AppCompatActivity {
         logAndAppend(ON_DESTROY);
     }
 
-    // TODO (2) Override onSaveInstanceState
-    // Do steps 3 - 5 within onSaveInstanceState
-    // TODO (3) Call super.onSaveInstanceState
-    // TODO (4) Call logAndAppend with the ON_SAVE_INSTANCE_STATE String
-    // TODO (5) Put the text from the TextView in the outState bundle
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        logAndAppend(ON_SAVE_INSTANCE_STATE);
+        outState.putCharSequence(LIFECYCLE_CALLBACKS_TEXT_KEY, mLifecycleDisplay.getText());
+    }
 
     /**
      * Logs to the console and appends the lifecycle method name to the TextView so that you can
