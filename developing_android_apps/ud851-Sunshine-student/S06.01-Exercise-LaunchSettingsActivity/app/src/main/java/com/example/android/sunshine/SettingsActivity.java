16f3c68a49ec4de22a6c8c458a5dff6d84e1f8e0
Lara Martin <larikimc@gmail.com>
2017-02-05 16:40:44 +0100

    go to correct screen when pressing back

--- developing_android_apps/ud851-Sunshine-student/S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
+++ developing_android_apps/ud851-Sunshine-student/S06.01-Exercise-LaunchSettingsActivity/app/src/main/java/com/example/android/sunshine/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,5 +15,14 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
