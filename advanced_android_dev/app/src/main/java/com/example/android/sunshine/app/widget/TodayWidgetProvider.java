b5c41699adff01b9e236f14b1768ab511576ba04
Lara Martin <larikimc@gmail.com>
2017-03-04 18:48:32 +0100

    7.01 add widget with static data

---
+++ advanced_android_dev/app/src/main/java/com/example/android/sunshine/app/widget/TodayWidgetProvider.java
@@ -0,0 +1,31 @@
+package com.example.android.sunshine.app.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.example.android.sunshine.app.MainActivity;
+import com.example.android.sunshine.app.R;
+
+/**
+ * Created by lara on 04.03.17.
+ */
+
+public class TodayWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.widget_today_small);
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
