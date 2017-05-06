12b2c5d7d378b735864a328fd82707427d99d1a8
Lara Martin <lara.martin.carretero@gmail.com>
2017-05-06 22:37:13 +0200

    T10.04 exercise periodic sync with JobDispatcher

--- developing_android_apps/ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
+++ developing_android_apps/ud851-Exercises-student/Lesson10-Hydration-Reminder/T10.04-Exercise-PeriodicSyncWithJobDispatcher/app/src/main/java/com/example/android/background/sync/ReminderUtilities.java
@@ -16,31 +16,56 @@
 package com.example.android.background.sync;
 
 
-public class ReminderUtilities {
-    // TODO (15) Create three constants and one variable:
-    //  - REMINDER_INTERVAL_SECONDS should be an integer constant storing the number of seconds in 15 minutes
-    //  - SYNC_FLEXTIME_SECONDS should also be an integer constant storing the number of seconds in 15 minutes
-    //  - REMINDER_JOB_TAG should be a String constant, storing something like "hydration_reminder_tag"
-    //  - sInitialized should be a private static boolean variable which will store whether the job
-    //    has been activated or not
+import android.content.Context;
 
-    // TODO (16) Create a synchronized, public static method called scheduleChargingReminder that takes
-    // in a context. This method will use FirebaseJobDispatcher to schedule a job that repeats roughly
-    // every REMINDER_INTERVAL_SECONDS when the phone is charging. It will trigger WaterReminderFirebaseJobService
-    // Checkout https://github.com/firebase/firebase-jobdispatcher-android for an example
-        // TODO (17) If the job has already been initialized, return
-        // TODO (18) Create a new GooglePlayDriver
-        // TODO (19) Create a new FirebaseJobDispatcher with the driver
-        // TODO (20) Use FirebaseJobDispatcher's newJobBuilder method to build a job which:
-            // - has WaterReminderFirebaseJobService as it's service
-            // - has the tag REMINDER_JOB_TAG
-            // - only triggers if the device is charging
-            // - has the lifetime of the job as forever
-            // - has the job recur
-            // - occurs every 15 minutes with a window of 15 minutes. You can do this using a
-            //   setTrigger, passing in a Trigger.executionWindow
-            // - replaces the current job if it's already running
-        // Finally, you should build the job.
-        // TODO (21) Use dispatcher's schedule method to schedule the job
-        // TODO (22) Set sInitialized to true to mark that we're done setting up the job
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.Driver;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.Trigger;
+
+import java.util.concurrent.TimeUnit;
+
+public class ReminderUtilities {
+
+    public static int REMINDER_INTERVAL_MINUTES = 15;
+    public static int REMINDER_INTERVAL_SECONDS = (int) TimeUnit.MINUTES.toSeconds(
+            REMINDER_INTERVAL_MINUTES);
+    public static int SYNC_FLEXTIME_SECONDS;
+    public static String REMINDER_JOB_TAG = "hydration_reminder_tag";
+    private static boolean sInitialized;
+
+    // synchronized to not have this method executed more than once at the time
+    synchronized public static void scheduleChargingReminder(Context context) {
+        if (sInitialized) {
+            return;
+        }
+        Driver driver = new GooglePlayDriver(context);
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(driver);
+        Job job = dispatcher.newJobBuilder()
+                // the JobService that will be called
+                .setService(WaterReminderFirebaseJobService.class)
+                // uniquely identifies the job
+                .setTag(REMINDER_JOB_TAG)
+                // constraints that need to be satisfied for the job to run
+                .setConstraints(
+                        // only run when the device is charging
+                        Constraint.DEVICE_CHARGING
+                )
+                // execute job forever even between reboots
+                .setLifetime(Lifetime.FOREVER)
+                // one-off job
+                .setRecurring(true)
+                // start between 0 and 15 minutes (900 seconds)
+                .setTrigger(Trigger.executionWindow(
+                        REMINDER_INTERVAL_SECONDS,
+                        REMINDER_INTERVAL_SECONDS + SYNC_FLEXTIME_SECONDS))
+                // overwrite an existing job with the same tag
+                .setReplaceCurrent(true)
+                .build();
+        dispatcher.schedule(job);
+        sInitialized = true;
+    }
 }
