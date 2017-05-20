ff73e2bb7ba0fbef51a0edf9762bfdc3e4d7393d
Lara Martin <larikimc@gmail.com>
2017-05-20 20:23:04 +0200

    TESP.02 exercise add MenuActivityScreenTest, test click on gridView

--- advanced_android_dev_course_2017/tea_time/TESP.02-Exercise-AddMenuActivityScreenTest/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
+++ advanced_android_dev_course_2017/tea_time/TESP.02-Exercise-AddMenuActivityScreenTest/app/src/androidTest/java/com/example/android/teatime/MenuActivityScreenTest.java
@@ -16,6 +16,21 @@
 
 package com.example.android.teatime;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+
 /**
  * This test demos a user clicking on a GridView item in MenuActivity which opens up the
  * corresponding OrderActivity.
@@ -26,16 +41,15 @@ package com.example.android.teatime;
  */
 
 
-// TODO (1) Add annotation to specify AndroidJUnitRunner class as the default test runner
+@RunWith(AndroidJUnit4.class)
 public class MenuActivityScreenTest {
 
-    // TODO (2) Add the rule that provides functional testing of a single activity
+    @Rule
+    public ActivityTestRule<MenuActivity> mMenuActivity = new ActivityTestRule<>(MenuActivity.class);
 
-    // TODO (3) Finish writing this test which will click on a gridView Tea item and verify that
-    // the OrderActivity opens up with the correct tea name displayed.
-
+    @Test
     public void clickGridViewItem_OpensOrderActivity() {
-
+        onData(anything()).inAdapterView(withId(R.id.tea_grid_view)).atPosition(0).perform(click());
+        onView(withId(R.id.tea_name_text_view)).check(matches(withText("Black Tea")));
     }
-
 }
